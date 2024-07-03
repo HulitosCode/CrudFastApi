a02b16801708f4615307cbeefd15d69ed5c92e73
HulitosCode <heltonmatimbe8@gmail.com>
2024-07-03 15:42:07 +0200

    Adicionando migracao no projecto

--- App/models.py
+++ App/models.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from enum import Enum
-from sqlalchemy import Column, String, Integer, Boolean, Text, ForeignKey, Enum as SqlENum
+from sqlalchemy import Column, String, Integer, Boolean, Text, ForeignKey, Enum as SqlENum, DateTime, func
 from sqlalchemy.orm import relationship
 from App.database import Base
 
@@ -18,6 +19,8 @@ class User(Base):
     username = Column(String(25), unique=True)
     email = Column(String(80), unique=True)
     password = Column(Text, nullable=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     todos = relationship('Todo', back_populates='user', cascade='all, delete-orphan')
 
@@ -29,6 +32,8 @@ class Todo(Base):
     title = Column(String(255), nullable=True)
     description = Column(String(255), nullable=True)
     state = Column(SqlENum(TodoState), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
